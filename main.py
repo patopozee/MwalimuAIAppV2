3651f8d9a1233f7a5f536b27ecb3cf7840ad9233
patopozee <mugopatrick13@gmail.com>
2026-08-09 20:24:47 +0300

    Fixing google sign in 14

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ if is_local:
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 else:
     # 💥 ALWAYS INCLUDE TRAILING SLASH IN PRODUCTION
-    REDIRECT_URI = "https://app.mwalimuaiapp.com/"
+    REDIRECT_URI = "https://mwalimuaiapp.com"
     if "OAUTHLIB_INSECURE_TRANSPORT" in os.environ:
         del os.environ["OAUTHLIB_INSECURE_TRANSPORT"]
 
