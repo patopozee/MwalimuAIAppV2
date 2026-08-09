59d72133ad24a554da466ec893793be310d9da86
patopozee <mugopatrick13@gmail.com>
2026-08-09 21:28:47 +0300

    Fixing google sign in 18

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ if is_local:
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 else:
     # FIX: Points to your custom domain with a trailing slash to match the config above
-    REDIRECT_URI = "https://mwalimuaiapp.com"
+    REDIRECT_URI = "https://app.mwalimuaiapp.com"
     if "OAUTHLIB_INSECURE_TRANSPORT" in os.environ:
         del os.environ["OAUTHLIB_INSECURE_TRANSPORT"]
 
